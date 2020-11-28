9925236df9730f52c6f6a2b048109ba75e03d83f
Starkin28 <victoreq00@gmail.com>
2020-11-28 20:59:00 +0100

    created data file, excalidraw and planning excel

---
+++ data.js
@@ -0,0 +1,24 @@
+[
+    {
+        name: "Challenge Title",
+        description: "First Challenge in order to prove recibing data",
+        miniDescription: "The First one",
+        category: "Children",
+        target: 2000,
+        collected: 100,
+        participants: 3,
+        days: 30,
+        creator: "Victor",
+    },
+    {
+        name: "Second Challenge Title",
+        description: "Second Challenge in order to prove recibing data",
+        miniDescription: "The Second one",
+        category: "Old People",
+        target: 5000,
+        collected: 2700,
+        participants: 15,
+        days: 19,
+        creator: "Victor",
+    }
+]
